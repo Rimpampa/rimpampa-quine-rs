84b1a430f1d3bb9b9c606da54ece4e5d1de5dab7
Riccardo Ripanti <35630802+Rimpampa@users.noreply.github.com>
2020-07-28 16:54:35 +0200

    Create comment.rs

---
+++ comment.rs
@@ -0,0 +1,6 @@
+/*  */
+fn main() {
+	let t = "";
+	let s=r#"println!("/* {} */\nfn main() {{\n\tlet t = \"{}\";\n\tlet s=r#\"{}\"{}#;{}\n}}\n/* {} */",t,t,s,"",s,t)"#;println!("/* {} */\nfn main() {{\n\tlet t = \"{}\";\n\tlet s=r#\"{}\"{}#;{}\n}}\n/* {} */",t,t,s,"",s,t)
+}
+/*  */
